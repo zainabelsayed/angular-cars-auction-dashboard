e266f0cb245e6f42fcaf097bef203f7ff898a33a
zainab-elsayed <zainab.elsayed@naj.ae>
2024-10-08 15:23:16 +0400

    intialize project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,149 @@
+import { Route } from '@angular/router';
+import { initialDataResolver } from 'app/app.resolvers';
+import { AuthGuard } from 'app/core/auth/guards/auth.guard';
+import { NoAuthGuard } from 'app/core/auth/guards/noAuth.guard';
+import { LayoutComponent } from 'app/layout/layout.component';
+
+// @formatter:off
+/* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+export const appRoutes: Route[] = [
+    // Redirect empty path to '/dashboards/project'
+    { path: '', pathMatch: 'full', redirectTo: 'apps/users' },
+
+    // Redirect signed-in user to the '/dashboards/project'
+    //
+    // After the user signs in, the sign-in page will redirect the user to the 'signed-in-redirect'
+    // path. Below is another redirection for that path to redirect the user to the desired
+    // location. This is a small convenience to keep all main routes together here on this file.
+    {
+        path: 'signed-in-redirect',
+        pathMatch: 'full',
+        redirectTo: 'apps/users',
+    },
+
+    // Auth routes for guests
+    {
+        path: '',
+        canActivate: [NoAuthGuard],
+        canActivateChild: [NoAuthGuard],
+        component: LayoutComponent,
+        data: {
+            layout: 'empty',
+        },
+        children: [
+            {
+                path: 'confirmation-required',
+                loadChildren: () =>
+                    import(
+                        'app/modules/auth/confirmation-required/confirmation-required.routes'
+                    ),
+            },
+            {
+                path: 'forgot-password',
+                loadChildren: () =>
+                    import(
+                        'app/modules/auth/forgot-password/forgot-password.routes'
+                    ),
+            },
+            {
+                path: 'reset-password',
+                loadChildren: () =>
+                    import(
+                        'app/modules/auth/reset-password/reset-password.routes'
+                    ),
+            },
+            {
+                path: 'sign-in',
+                loadChildren: () =>
+                    import('app/modules/auth/sign-in/sign-in.routes'),
+            },
+            {
+                path: 'sign-up',
+                loadChildren: () =>
+                    import('app/modules/auth/sign-up/sign-up.routes'),
+            },
+        ],
+    },
+
+    // Auth routes for authenticated users
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component: LayoutComponent,
+        data: {
+            layout: 'empty',
+        },
+        children: [
+            {
+                path: 'sign-out',
+                loadChildren: () =>
+                    import('app/modules/auth/sign-out/sign-out.routes'),
+            },
+            {
+                path: 'unlock-session',
+                loadChildren: () =>
+                    import(
+                        'app/modules/auth/unlock-session/unlock-session.routes'
+                    ),
+            },
+        ],
+    },
+
+    // Landing routes
+    {
+        path: '',
+        component: LayoutComponent,
+        data: {
+            layout: 'empty',
+        },
+        children: [
+            {
+                path: 'home',
+                loadChildren: () =>
+                    import('app/modules/landing/home/home.routes'),
+            },
+        ],
+    },
+
+    // Admin routes
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        canActivateChild: [AuthGuard],
+        component: LayoutComponent,
+        resolve: {
+            initialData: initialDataResolver,
+        },
+        children: [
+            // Apps
+            {
+                path: 'apps',
+                children: [
+                    {
+                        path: 'users',
+                        loadChildren: () =>
+                            import('app/modules/admin/apps/users/users.routes'),
+                    },
+                    {
+                        path: 'cars',
+                        loadChildren: () =>
+                            import('app/modules/admin/apps/cars/cars.routes'),
+                    },
+                ],
+            },
+
+            // 404 & Catch all
+            {
+                path: '404-not-found',
+                pathMatch: 'full',
+                loadChildren: () =>
+                    import(
+                        'app/modules/admin/pages/error/error-404/error-404.routes'
+                    ),
+            },
+            { path: '**', redirectTo: '404-not-found' },
+        ],
+    },
+];
---
+++ src/app/mock-api/common/navigation/data.ts
@@ -0,0 +1,63 @@
+/* eslint-disable */
+import { FuseNavigationItem } from '@fuse/components/navigation';
+
+export const defaultNavigation: FuseNavigationItem[] = [
+    {
+        id: 'apps',
+        title: 'Applications',
+        subtitle: 'Custom made application designs',
+        type: 'group',
+        icon: 'heroicons_outline:home',
+        children: [
+            {
+                id: 'apps.users',
+                title: 'Users',
+                type: 'basic',
+                icon: 'heroicons_outline:user-group',
+                link: '/apps/users',
+            },
+            {
+                id: 'apps.cars',
+                title: 'Cars',
+                type: 'collapsable',
+                icon: 'heroicons_outline:truck',
+                children: [
+                    {
+                        id: 'apps.cars.inventory',
+                        title: 'Inventory',
+                        type: 'basic',
+                        link: '/apps/cars/inventory',
+                    },
+                ],
+            },
+        ],
+    },
+];
+
+export const compactNavigation: FuseNavigationItem[] = [
+    {
+        id: 'apps',
+        title: 'Apps',
+        tooltip: 'Apps',
+        type: 'aside',
+        icon: 'heroicons_outline:squares-2x2',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+];
+export const futuristicNavigation: FuseNavigationItem[] = [
+    {
+        id: 'apps',
+        title: 'APPS',
+        type: 'group',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+];
+export const horizontalNavigation: FuseNavigationItem[] = [
+    {
+        id: 'apps',
+        title: 'Apps',
+        type: 'group',
+        icon: 'heroicons_outline:squares-2x2',
+        children: [], // This will be filled from defaultNavigation so we don't have to manage multiple sets of the same navigation
+    },
+];
